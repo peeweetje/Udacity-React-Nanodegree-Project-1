6757e8e1ff1cd6231b6dd81181c03da69e8537fc
peeweetje <petravos70@gmail.com>
2021-01-12 20:22:37 +0100

    Update the trigger of the existing workflow as suggested above to run on new pull requests to master

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 
-/** 
- this test is not failing locally but on build it is, it is not updating
-**/
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
