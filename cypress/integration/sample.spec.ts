f5fdcf2ba11b01144a5148a82b07d6041bb059d3
PEEWEE\petra <petravos70@gmail.com>
2021-07-11 09:05:50 +0200

    Add test to run page on localhost 3000. Remove code no longer needed.

---
+++ cypress/integration/sample.spec.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Go to the correct page', () => {
+    it('Visits the correct page', () => {
+      cy.visit('http://localhost:3000/')
+      
+    })
+  })
+
+ 
